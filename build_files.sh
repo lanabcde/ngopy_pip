a26edc17fc331502a3a328c4e8f9e7a4fc280598
lanabcde <144417094+lanabcde@users.noreply.github.com>
2024-05-01 02:46:00 +0800

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -4,13 +4,13 @@
 # Pastikan bahwa Anda telah mengatur PIPENV_ACTIVE sebelumnya
 # Misalnya, dengan menjalankan "pipenv shell" sebelumnya
 if [ -z "$PIPENV_ACTIVE" ]; then
-    echo "Error: Pipenv environment is not activated."
-    exit 1
+    echo "Error: Pipenv environment tidak aktif. aktifkan sekarang..."
+    pipenv shell || exit 1
 fi
 
 # Install dependensi dari Pipfile.lock
-pipenv install --deploy --ignore-pipfile
+pipenv install --deploy --ignore-pipfile || exit 1
 
 # Jalankan langkah tambahan build jika diperlukan
 # Contohnya, kumpulkan berkas statis
-pipenv run python manage.py collectstatic
+pipenv run python manage.py collectstatic || exit 1
